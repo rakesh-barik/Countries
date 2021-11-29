c840643cbba34d77e9132d3fa526217a7e420b3f
rakesh barik <stunningandroid@gmail.com>
2021-11-29 15:52:27 +0530

    UI - Card corner color issue

--- app/src/main/java/com/tinybinlabs/countries/presentation/country/CountryDetailScreen.kt
+++ app/src/main/java/com/tinybinlabs/countries/presentation/country/CountryDetailScreen.kt
@@ -1,6 +1,5 @@
 package com.tinybinlabs.countries.presentation.country
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -10,7 +9,6 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -51,10 +49,8 @@ fun CountryDetailScreen(
         Card(
             modifier = Modifier
                 .wrapContentSize()
-                .padding(24.dp)
-                .background(Color.LightGray),
+                .padding(24.dp),
             elevation = 4.dp
-
         ) {
             Column(
                 modifier = Modifier
